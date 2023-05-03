9994bedb12b754cd595b8a0c53c2f0b67a99dcac
jgapadua <joaogabrielpadua@gmail.com>
2023-05-03 10:53:50 -0300

    como funciona types

--- index.ts
+++ index.ts
@@ -1,25 +1,34 @@
-function soma(a:number, b:number){
+/* function soma(a:number, b:number){
   return a + b;
-}
+} */
 
 //types
 //interfaces
 interface IAnimal{
   nome:string;
   tipo:'terrestre' | 'aquático';
-  executarRugido(alturaEmDecibeis:number):void;
+  //executarRugido(alturaEmDecibeis:number):void;
+  domestico: boolean;
 }
+/* type IAnimal={
+  nome:string;
+  tipo:'terrestre' | 'aquático';
+  executarRugido(alturaEmDecibeis:number):void;
+} */
 interface IFelino extends IAnimal{
   visaoNotura: boolean;
+}
+interface ICanino extends IAnimal{
+  porte:'pequeno' | 'medio' | 'grande';
 }
 const animal: IAnimal={
   nome: 'Elefante',
   tipo: 'terrestre',
-  executarRugido:(alturaEmDecibeis)=>(`${alturaEmDecibeis}dB`)
+  //executarRugido:(alturaEmDecibeis)=>(`${alturaEmDecibeis}dB`)
 }
-animal.executarRugido('')
-const felino: IFelino={
+//animal.executarRugido('')
+/* const felino: IFelino={
   nome:'Leão',
   tipo:'terrestre',
   visaoNotura: true,
-}
+} */
