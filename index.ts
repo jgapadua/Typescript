907fc3fc62a7037d2d64f5687b9a95c332b1ba00
jgapadua <joaogabrielpadua@gmail.com>
2023-05-03 10:44:03 -0300

    interfaces

--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
 function soma(a:number, b:number){
   return a + b;
 }
+
+//types
+//interfaces
+interface IAnimal{
+  nome:string;
+  tipo:'terrestre' | 'aquático';
+  executarRugido(alturaEmDecibeis:number):void;
+}
+interface IFelino extends IAnimal{
+  visaoNotura: boolean;
+}
+const animal: IAnimal={
+  nome: 'Elefante',
+  tipo: 'terrestre',
+  executarRugido:(alturaEmDecibeis)=>(`${alturaEmDecibeis}dB`)
+}
+animal.executarRugido('')
+const felino: IFelino={
+  nome:'Leão',
+  tipo:'terrestre',
+  visaoNotura: true,
+}
