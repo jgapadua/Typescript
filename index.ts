fbe0a699ced7a956feff3b9f5ade827d645e82f2
jgapadua <joaogabrielpadua@gmail.com>
2023-05-03 11:10:47 -0300

    tag input

--- index.ts
+++ index.ts
@@ -1,34 +1,49 @@
 /* function soma(a:number, b:number){
   return a + b;
-} */
+} 
 
 //types
 //interfaces
 interface IAnimal{
   nome:string;
   tipo:'terrestre' | 'aquático';
-  //executarRugido(alturaEmDecibeis:number):void;
+  executarRugido(alturaEmDecibeis:number):void;
   domestico: boolean;
 }
-/* type IAnimal={
+
+ type IAnimal={
   nome:string;
   tipo:'terrestre' | 'aquático';
   executarRugido(alturaEmDecibeis:number):void;
-} */
+} 
+
 interface IFelino extends IAnimal{
   visaoNotura: boolean;
 }
+
 interface ICanino extends IAnimal{
   porte:'pequeno' | 'medio' | 'grande';
 }
-const animal: IAnimal={
-  nome: 'Elefante',
+
+type IDomestico = IFelino | ICanino;
+
+const animal: IDomestico={
+  domestico: true,
+  nome: 'Cachorro',
+  porte: 'medio',
   tipo: 'terrestre',
-  //executarRugido:(alturaEmDecibeis)=>(`${alturaEmDecibeis}dB`)
+  executarRugido:(alturaEmDecibeis)=>(`${alturaEmDecibeis}dB`)
 }
-//animal.executarRugido('')
-/* const felino: IFelino={
+
+animal.executarRugido('')
+
+ const felino: IFelino={
   nome:'Leão',
   tipo:'terrestre',
   visaoNotura: true,
 } */
+const input = document.getElementById('input') as HTMLInputElement;
+input.addEventListener('input',(event)=>{
+  const i = event.currentTarget as HTMLInputElement;
+   console.log(i.value)
+});
